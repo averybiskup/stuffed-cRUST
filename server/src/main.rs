03ca6c607038f4607549752abce72f857cede5b2
Avery Biskup <averybiskup@gmail.com>
2022-11-06 20:07:04 -0800

    Sent response to client

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
 
 fn main() {
     // unwrap stops program if errors happen
@@ -11,6 +14,19 @@ fn main() {
         // unwrap -> terminate if stream has any errors
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()                                // returns iterator
+        .map(|result| result.unwrap())          // map and unwrap each result
+        .take_while(|line| !line.is_empty())
+        .collect();                             // add lines to vec
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
