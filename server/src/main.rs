3d56a4955a9409f4fcf6d625681af6eff6e80a1a
Avery Biskup <averybiskup@gmail.com>
2022-11-07 19:37:20 -0800

    Added multithreaded ability

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,41 @@
+use server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
 
 fn main() {
     // unwrap stops program if errors happen
     // listener becomes a sequence of streams of type TcpStream
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    // process each connection and allow us to handle each stream
     for stream in listener.incoming() {
-
-        // unwrap -> terminate if stream has any errors
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
+
     }
+
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -37,5 +46,4 @@ fn handle_connection(mut stream: TcpStream) {
     );
 
     stream.write_all(response.as_bytes()).unwrap();
-
 }
